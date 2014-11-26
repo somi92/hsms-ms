16ecf83c0f9c3935188241867f411f7800876b35
Milos Stojanovic <milos92s@yahoo.com>
2014-11-26 22:54:53 +0100

    Refactored js and moved do separate file. Started working on update command. Need to fetch right org id to automatically fill in form.

--- app/views/data/hsms.php
+++ app/views/data/hsms.php
@@ -14,7 +14,7 @@ view/data/hsms:
   <link rel="stylesheet" type="text/css" href="../../../public/css/style.css">
   <script type="text/javascript" charset="utf8" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
   <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.4/js/jquery.dataTables.js"></script>
-  <script type="text/javascript" charset="utf8" src="../../../public/js/session.js"></script>
+  <script type="text/javascript" charset="utf8" src="../../../public/js/script.js"></script>
 
   <head>
     <title>HSMS Management System</title>
@@ -89,166 +89,16 @@ view/data/hsms:
         // $obj = $data["actions"];
         // $json = json_encode($data);
         echo '<button id="popup" onclick="div_show()">Dodaj novu akciju</button>';
-        echo '<button id="popup" onclick="div_show()">Izmeni</button>';
+        echo '<button id="btn_update" >Izmeni</button>';
         echo '<button id="btn_delete">Obrisi</button>';
         echo '</br></br><a href="/HSMS-MS/public/home/logout">Logout</a>';
       }
 
   	?>
 
-    <script>
-
-      var arr = <?php echo json_encode($data["actions"]) ?>;
-      
-      function check_empty() {
-      
-        if (document.getElementById('desc').value == "" || document.getElementById('number').value == "" || document.getElementById('price').value == "") {
-          alert("Morate popuniti opis, broj i cenu!");
-        } else {
-          
-          var descF = $("#desc").val();
-          var numberF = $("#number").val();
-          var priceF = $("#price").val();
-          var statusF = $("#status").val();
-          var organisationF = $("#organisation").val();
-          var priorityF = $("#priority").val();
-          var remarkF = $("#remark").val();
-
-          $.post("/HSMS-MS/public/data/insert",{
-            desc: descF,
-            number: numberF,
-            price: priceF,
-            status: statusF,
-            organisation: organisationF,
-            priority: priorityF,
-            remark: remarkF
-          },
-
-          function(data) {
-            // alert(data);
-            document.getElementById('form').submit();
-            $(document).loadTable();
-          });
-        }
-      }
-
-      function div_show() {
-        document.getElementById('abc').style.display = "block";
-        $.post("/HSMS-MS/public/data/query",{
-            query_target: "ORGANIZACIJA"
-          },function(data){
-              // alert(data);
-              var obj = jQuery.parseJSON(data);
-              for(var i=0; i<obj.length; i++) {
-                var node = document.createElement("OPTION");
-                node.setAttribute("value",obj[i]['id']);
-                var textnode = document.createTextNode(obj[i]['name']+"");
-                node.appendChild(textnode);
-                document.getElementById("organisation").appendChild(node);
-              } 
-            });  
-  // document.getElementById('organisation').inner
-}
-
-      function div_org_load() {
-
-      }
-
-      function div_hide(){
-        document.getElementById('abc').style.display = "none";
-      }
-
-    </script>
-
     <script type="text/javascript">
-
-      var table;
       var arr = <?php echo json_encode($data["actions"]) ?>;
-      $(document).ready(function() {
 
-        $(document).loadTable();
-      
-        $('#hsms tbody').on( 'click', 'tr', function () {
-          
-          if ( $(this).hasClass('selected') ) {
-            $(this).removeClass('selected');
-          } else {
-              table.$('tr.selected').removeClass('selected');
-              $(this).addClass('selected');
-            }
-        } );
-         
-        $('#btn_delete').click( function () {
-          // table.row('.selected').remove().draw( false );
-          if(table.row('.selected').data() == null) {
-            alert("Izaberite red tabele koji zelite izbrisati.");
-          } else {
-              if(window.confirm("Jeste li sigurni?")) {
-                var id = table.row('.selected').data().id;
-                $.post("/HSMS-MS/public/data/delete",{
-                    delete_table: "HUMANITARNI_BROJ",
-                    delete_id: id
-                },
-
-                function(data) {
-                  // alert(data);
-                  // document.getElementById('form').submit();
-                  // $(document).loadTable();
-                  // if(data == "ok") {
-                  table.row('.selected').remove().draw( false );
-                  // alert("OKKKKKKK");
-                  // } else {
-                  // alert("ERROOOR");
-                  // }
-                });
-              }
-                  // alert(table.row('.selected').data().remark);  
-            }  
-        });
-      });
-
-      
-
-      (function( $ ){
-
-         $.fn.loadTable = function() {
-
-            table = $('#hsms').DataTable({
-              retrieve: true,
-              data: arr,
-              columns: [
-                  { data: 'id' },
-                  { data: 'desc' },
-                  { data: 'number'},
-                  { data: 'price'},
-                  { data: 'status'},
-                  { data: 'organisation'},
-                  { data: 'web'},
-                  { data: 'priority'},
-                  { data: 'remark'}
-              ],
-              searching: false,
-              ordering:  false,
-              language: {
-                "sProcessing":   "Procesiranje u toku...",
-                "sLengthMenu":   "Prikaži _MENU_ elemenata",
-                "sZeroRecords":  "Nije pronađen nijedan rezultat",
-                "sInfo":         "Prikaz _START_ do _END_ od ukupno _TOTAL_ elemenata",
-                "sInfoEmpty":    "Prikaz 0 do 0 od ukupno 0 elemenata",
-                "sInfoFiltered": "(filtrirano od ukupno _MAX_ elemenata)",
-                "sInfoPostFix":  "",
-                "sSearch":       "Pretraga:",
-                "sUrl":          "",
-                "oPaginate": {
-                    "sFirst":    "Početna",
-                    "sPrevious": "Prethodna",
-                    "sNext":     "Sledeća",
-                    "sLast":     "Poslednja"
-                } 
-              }
-          });
-        }; 
-      })( jQuery );
     </script>
 
     <div id="abc">
---
+++ public/js/script.js
@@ -0,0 +1,180 @@
+function check_empty() {
+      
+  if (document.getElementById('desc').value == "" || document.getElementById('number').value == "" || document.getElementById('price').value == "") {
+    alert("Morate popuniti opis, broj i cenu!");
+  } else {
+      var descF = $("#desc").val();
+      var numberF = $("#number").val();
+      var priceF = $("#price").val();
+      var statusF = $("#status").val();
+      var organisationF = $("#organisation").val();
+      var priorityF = $("#priority").val();
+      var remarkF = $("#remark").val();
+
+      $.post("/HSMS-MS/public/data/insert",{
+          desc: descF,
+          number: numberF,
+          price: priceF,
+          status: statusF,
+          organisation: organisationF,
+          priority: priorityF,
+          remark: remarkF
+      	},
+
+        function(data) {
+          // alert(data);
+          document.getElementById('form').submit();
+          $(document).loadTable();
+        }
+      );
+    }
+}
+
+function div_show() {
+  document.getElementById('abc').style.display = "block";
+  $.post("/HSMS-MS/public/data/query",{
+      query_target: "ORGANIZACIJA"
+    },
+
+    function(data){
+      // alert(data);
+      var obj = jQuery.parseJSON(data);
+      for(var i=0; i<obj.length; i++) {
+        var node = document.createElement("OPTION");
+        node.setAttribute("value",obj[i]['id']);
+        var textnode = document.createTextNode(obj[i]['name']+"");
+        node.appendChild(textnode);
+        document.getElementById("organisation").appendChild(node);
+      } 
+    });  
+  // document.getElementById('organisation').inner
+}
+
+function div_org_load() {
+
+}
+
+function div_hide(){
+
+	$("#desc").val("");
+  $("#number").val("");
+  $("#price").val("");
+  $("#status").val("");
+	$("#organisation").val("");
+  $("#priority").val("");
+	$("#remark").val("");
+
+  document.getElementById('abc').style.display = "none";
+}
+
+var table;
+$(document).ready(function() {
+
+  $(document).loadTable();
+      
+  $('#hsms tbody').on( 'click', 'tr', function () {
+          
+    if ( $(this).hasClass('selected') ) {
+      $(this).removeClass('selected');
+    } else {
+      	table.$('tr.selected').removeClass('selected');
+        $(this).addClass('selected');
+      }
+  } );
+         
+  $('#btn_delete').click( function () {
+    // table.row('.selected').remove().draw( false );
+    if(table.row('.selected').data() == null) {
+      alert("Izaberite red tabele koji zelite izbrisati.");
+    } else {
+        if(window.confirm("Jeste li sigurni?")) {
+          var id = table.row('.selected').data().id;
+          $.post("/HSMS-MS/public/data/delete",{
+              delete_table: "HUMANITARNI_BROJ",
+              delete_id: id
+          },
+
+          function(data) {
+            // alert(data);
+            // document.getElementById('form').submit();
+            // $(document).loadTable();
+            // if(data == "ok") {
+            table.row('.selected').remove().draw( false );
+            // alert("OKKKKKKK");
+            // } else {
+            // alert("ERROOOR");
+            // }
+          });
+        }
+        // alert(table.row('.selected').data().remark);  
+      }  
+	});
+
+	$('#btn_update').click( function() {
+		if(table.row('.selected').data() == null) {
+			alert("Izaberite red tabele koji zelite izmeniti.");
+		} else {
+				
+				// alert(table.row('.selected').data().web);
+				var id = table.row('.selected').data().id;
+				var desc = table.row('.selected').data().desc;
+				var number = table.row('.selected').data().number;
+				var price = table.row('.selected').data().price;
+				var status = table.row('.selected').data().status;
+				var organisation = table.row('.selected').data().organisation;
+				var web = table.row('.selected').data().web;
+				var priority = table.row('.selected').data().priority;
+				var remark = table.row('.selected').data().remark;
+
+				$("#desc").val(desc);
+	      $("#number").val(number);
+	      $("#price").val(price);
+	      $("#status").val(status);
+	      $("#organisation").val(organisation);
+	      $("#priority").val(priority);
+	      $("#remark").val(remark);
+	      div_show();
+		}
+	});
+});
+
+(function( $ ){
+
+  $.fn.loadTable = function() {
+
+  	table = $('#hsms').DataTable({
+      retrieve: true,
+      data: arr,
+      columns: [
+        { data: 'id' },
+        { data: 'desc' },
+        { data: 'number'},
+        { data: 'price'},
+        { data: 'status'},
+        { data: 'organisation'},
+        { data: 'web'},
+        { data: 'priority'},
+       	{ data: 'remark'}],
+
+        searching: false,
+        ordering:  false,
+        language: {
+          "sProcessing":   "Procesiranje u toku...",
+          "sLengthMenu":   "Prikaži _MENU_ elemenata",
+          "sZeroRecords":  "Nije pronađen nijedan rezultat",
+          "sInfo":         "Prikaz _START_ do _END_ od ukupno _TOTAL_ elemenata",
+          "sInfoEmpty":    "Prikaz 0 do 0 od ukupno 0 elemenata",
+          "sInfoFiltered": "(filtrirano od ukupno _MAX_ elemenata)",
+          "sInfoPostFix":  "",
+          "sSearch":       "Pretraga:",
+          "sUrl":          "",
+          "oPaginate": {
+            "sFirst":    "Početna",
+            "sPrevious": "Prethodna",
+            "sNext":     "Sledeća",
+            "sLast":     "Poslednja"
+          } 
+        }
+    });
+  }; 
+})( jQuery );
